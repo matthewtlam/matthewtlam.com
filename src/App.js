e75b69481136eca639a070898ce2cb67b38784c7
Matthew Lam <m39lam@uwaterloo.ca>
2022-05-07 18:40:58 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import profilepic from './matthewprofile.png';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={profilepic} alt="Profile Picture" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi, I'm Matthew Lam
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      
+        <p>
+        I'm a third year Computer Science from the University of Waterloo interested in product design, software architecture and project management. 
+        </p>
+        <p>
+        Some of my hobbies include bouldering, power lifting, playing video games and listening to podcasts. 
+        </p>
+        <p>
+        If you want to get in touch, you can contact me with any of the social media links on the site.
+        </p>
+        
+        
       </header>
     </div>
   );
